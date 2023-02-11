1dab02cb610b4eced7fff769d7ee5d9a27879f3b
mperkins1995 <mperkins1995@gmail.com>
2023-02-11 13:30:19 -0600

    added Vendor model and installed get_vendor app in tacodelimap settings

--- tacodelimap/get_vendors/models.py
+++ tacodelimap/get_vendors/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
