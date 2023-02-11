c1b03f1288cca08a2a0794df02c2df50a4b77ad6
mperkins1995 <mperkins1995@gmail.com>
2023-02-11 13:13:38 -0600

    created initial get_vendors view and wired URLconf

--- tacodelimap/tacodelimap/urls.py
+++ tacodelimap/tacodelimap/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('get_vendors/', include('get_vendors.urls')),
     path('admin/', admin.site.urls),
 ]
