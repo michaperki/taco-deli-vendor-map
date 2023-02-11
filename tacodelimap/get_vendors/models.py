1c67fd58fc429dd1e6e813b2b175def59c42df0d
mperkins1995 <mperkins1995@gmail.com>
2023-02-11 14:07:05 -0600

    added vendor names and address via web scrape

--- tacodelimap/get_vendors/models.py
+++ tacodelimap/get_vendors/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Vendor(models.Model):
     name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    state = models.CharField(max_length=200)
-    zip_code = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=200)
-    website = models.CharField(max_length=200)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    address = models.CharField(max_length=200, null=True, blank=True)
+    # city = models.CharField(max_length=200)
+    # state = models.CharField(max_length=200)
+    # zip_code = models.CharField(max_length=200)
+    # phone_number = models.CharField(max_length=200)
+    # website = models.CharField(max_length=200)
+    # latitude = models.FloatField()
+    # longitude = models.FloatField()
 
     def __str__(self):
         return self.name
--- tacodelimap/get_vendors/views.py
+++ tacodelimap/get_vendors/views.py
@@ -7,6 +7,12 @@ from bs4 import BeautifulSoup
 import googlemaps
 import pandas as pd
 
+from .models import Vendor
+
+import numpy as np
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
 def index(request):
     response = HttpResponse("Hello, world. You're at the taco deli vendor map index.") 
     return response
@@ -24,3 +30,20 @@ def get_list_of_vendors():
     df = df[df["Vendors"].str.match("^[a-zA-Z]")]
     df = df.reset_index(drop=True)
     return df
+
+def get_vendor_address(vendor):
+    geolocator = Nominatim(user_agent="tacodeli")
+    try:
+        location = geolocator.geocode(vendor)
+    except:
+        location = None
+    if location is None:
+        return np.nan
+    return location.address
+
+def create_vendor(df):
+    for index, row in df.iterrows():
+        vendor_name = row["Vendors"]
+        address = get_vendor_address(vendor_name)
+        vendor = Vendor(name=vendor_name, address=address)
+        vendor.save()
